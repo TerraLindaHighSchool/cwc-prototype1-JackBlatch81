32f5bb0d9514615fa9cacdef491afebe23c7637e
PoshSquash <57773399+PoshSquash@users.noreply.github.com>
2021-01-05 15:19:50 -0800

    Milestone 1.1 complete

---
+++ Assets/Challenge 1/Scripts/CameraControllerX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerX : MonoBehaviour
+{
+    public GameObject player;
+    
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + player.transform.forward * -13, 0.05f);
+        transform.LookAt(player.transform.position);
+    }
+}
--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -7,6 +7,8 @@ public class PlayerControllerX : MonoBehaviour
     public float speed;
     public float rotationSpeed;
     public float verticalInput;
+    public float horizontalInput;
+    public float rudderSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +21,25 @@ public class PlayerControllerX : MonoBehaviour
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
 
         // move the plane forward at a constant rate
         transform.Translate(Vector3.forward * speed);
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+
+        // tilt the plane left to right based on left and right keys
+        transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime * horizontalInput);
+
+        if (Input.GetKey("e"))
+        {
+            transform.Rotate(Vector3.up * rudderSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("q"))
+        {
+            transform.Rotate(Vector3.down * rudderSpeed * Time.deltaTime);
+        }
     }
 }
