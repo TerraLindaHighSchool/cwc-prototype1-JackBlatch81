0a796bd0bf999796ce7ea19c10cea79f2645ea41
PoshSquash <57773399+PoshSquash@users.noreply.github.com>
2021-01-07 15:46:24 -0800

    Milestone 1.3 complete

---
+++ Assets/Challenge 1/Scripts/MouseCursorX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursorX : MonoBehaviour
+{
+    public GameObject clickEffect;
+    public Sprite cursor;
+    private SpriteRenderer rend;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            rend.sprite = cursor;
+            Instantiate(clickEffect, transform.position, Quaternion.identity);
+        }
+    }
+}
--- Assets/Challenge 1/Scripts/PointContrtollerX.cs
+++ Assets/Challenge 1/Scripts/PointContrtollerX.cs
@@ -15,7 +15,7 @@ public class PointContrtollerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+ 
     }
 
     private void OnTriggerEnter(Collider other)
